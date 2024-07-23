2b47602915c419433a7a72cf0763997d0944ba23
jolzi <jv186@msn.com>
2024-07-23 16:22:06 +0200

    Probably not working update of gltfloader integration

--- threejs/geneticman/geneticman.js
+++ threejs/geneticman/geneticman.js
@@ -21,7 +21,6 @@ let generationIndex = 1;
 camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 20);
 camera.position.set(-5, 2, -5);
 
-
 clock = new THREE.Clock();
 
 scene = new THREE.Scene();
@@ -43,15 +42,12 @@ const controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.target.set(0, 0, 1);
 controls.update();
 
-
 // create an AudioListener and add it to the camera
 const listener = new THREE.AudioListener();
 camera.add(listener);
 
 // create a global audio source
-// create a global audio source
 const sounds = [];
-
 const sound = new THREE.PositionalAudio(listener);
 const sound2 = new THREE.PositionalAudio(listener);
 const sound3 = new THREE.PositionalAudio(listener);
@@ -60,44 +56,42 @@ const sound4 = new THREE.PositionalAudio(listener);
 // load a sound and set it as the Audio object's buffer
 const audioLoader = new THREE.AudioLoader();
 audioLoader.load('../threejs/data/bell.mp3', function (buffer) {
-	sound.setBuffer(buffer);
-	sound.setLoop(false);
-	sound.setVolume(1.3);
-	sounds.push(sound);
+    sound.setBuffer(buffer);
+    sound.setLoop(false);
+    sound.setVolume(1.3);
+    sounds.push(sound);
 });
 
 audioLoader.load('../threejs/data/bell2.mp3', function (buffer) {
-	sound2.setBuffer(buffer);
-	sound2.setLoop(false);
-	sound2.setVolume(1.3);
-	sounds.push(sound2);
+    sound2.setBuffer(buffer);
+    sound2.setLoop(false);
+    sound2.setVolume(1.3);
+    sounds.push(sound2);
 });
 
 audioLoader.load('../threejs/data/bell3.mp3', function (buffer) {
-	sound3.setBuffer(buffer);
-	sound3.setLoop(false);
-	sound3.setVolume(1.3);
-	sounds.push(sound3);
+    sound3.setBuffer(buffer);
+    sound3.setLoop(false);
+    sound3.setVolume(1.3);
+    sounds.push(sound3);
 });
 
 audioLoader.load('../threejs/data/bell4.mp3', function (buffer) {
-	sound4.setBuffer(buffer);
-	sound4.setLoop(false);
-	sound4.setVolume(1.3);
-	sounds.push(sound4);
+    sound4.setBuffer(buffer);
+    sound4.setLoop(false);
+    sound4.setVolume(1.3);
+    sounds.push(sound4);
 });
-
 
 const padListener = new THREE.AudioListener();
 camera.add(padListener);
 
 const padSound = new THREE.PositionalAudio(padListener);
-
 audioLoader.load('../threejs/data/pad.mp3', function (buffer) {
-	padSound.setBuffer(buffer);
-	padSound.setLoop(true);
-	padSound.setVolume(3.0);
-	padSound.play();
+    padSound.setBuffer(buffer);
+    padSound.setLoop(true);
+    padSound.setVolume(3.0);
+    padSound.play();
 });
 
 document.getElementById("generation").innerHTML = "Generation: " + generationIndex;
@@ -105,172 +99,132 @@ document.getElementById("generation").innerHTML = "Generation: " + generationInd
 // model
 const loader = new THREE.GLTFLoader();
 
-//Load Hau den Lukas
-
-
-
+// Load Hau den Lukas
 loader.load('../threejs/data/Lukas.glb', function (gltf) {
+    lukas = gltf.scene;
+    lukasCollider = lukas.getObjectByName('LukasCollider');
+    lukasCollider.material.transparent = true;
+    scene.add(lukas);
 
-	lukas = gltf.scene;
-	lukasCollider = lukas.getObjectByName('LukasCollider');
-	lukasCollider.material.transparent = true;
-	scene.add(lukas);
+    lukasCollider.add(sound);
 
-	lukasCollider.add(sound);
-	//console.log(lukasCollider);
-
-	lukasColliderPosition = new THREE.Vector3();
-	lukasColliderPosition.setFromMatrixPosition(lukasCollider.matrixWorld);
-
+    lukasColliderPosition = new THREE.Vector3();
+    lukasColliderPosition.setFromMatrixPosition(lukasCollider.matrixWorld);
 });
 
 loader.load('../threejs/data/MuscHammer.glb', function (gltf) {
-
-	setUpPersons(gltf);
-
+    setUpPersons(gltf);
 });
 
 function setUpPersons(gltf) {
-	let persons = [];
-	for (let i = 0; i < setSize; i++) {
+    let persons = [];
+    for (let i = 0; i < setSize; i++) {
+        persons[i] = new Person();
 
-		persons[i] = new Person();
+        if (i === 0) {
+            persons[i].mesh = gltf.scene;
+        } else {
+            persons[i].mesh = gltf.scene.clone(true);
 
+            persons[i].mesh.getObjectByName('Object1').material = gltf.scene.getObjectByName('Object1').material.clone();
+            persons[i].mesh.getObjectByName('Object1').material.transparent = true;
+            persons[i].mesh.getObjectByName('Object1').material.opacity = 0;
 
-		//TODO: make shit work.
+            persons[i].mesh.getObjectByName('11685_hammer_v1_L3_1').material = gltf.scene.getObjectByName('11685_hammer_v1_L3_1').material.clone();
+            persons[i].mesh.getObjectByName('11685_hammer_v1_L3_1').material.transparent = true;
+            persons[i].mesh.getObjectByName('11685_hammer_v1_L3_1').material.opacity = 0;
 
-		if (i === 0) {
-			persons[i].mesh = gltf.scene;
-		} else {
-			persons[i].mesh = gltf.scene.clone(true);
+            persons[i].mesh.getObjectByName('11685_hammer_v1_L3_2').material = gltf.scene.getObjectByName('11685_hammer_v1_L3_2').material.clone();
+            persons[i].mesh.getObjectByName('11685_hammer_v1_L3_2').material.transparent = true;
+            persons[i].mesh.getObjectByName('11685_hammer_v1_L3_2').material.opacity = 0;
+        }
 
-			//Make all but one (which will later be the fittest) be invisible;
-			persons[i].mesh.getObjectByName('Object1').material = gltf.scene.getObjectByName('Object1').material.clone();
-			persons[i].mesh.getObjectByName('Object1').material.transparent = true;
-			persons[i].mesh.getObjectByName('Object1').material.opacity = 0;
+        persons[i].rightArm = persons[i].mesh.getObjectByName('Bone');
+        persons[i].collider = persons[i].mesh.getObjectByName('Collider');
+        persons[i].collider.material.transparent = true;
 
+        let rotationKF = new THREE.QuaternionKeyframeTrack('.quaternion', persons[i].times, persons[i].values);
+        persons[i].tracks = [rotationKF];
 
-			persons[i].mesh.getObjectByName('11685_hammer_v1_L3_1').material = gltf.scene.getObjectByName('11685_hammer_v1_L3_1').material.clone();
-			persons[i].mesh.getObjectByName('11685_hammer_v1_L3_1').material.transparent = true;
-			persons[i].mesh.getObjectByName('11685_hammer_v1_L3_1').material.opacity = 0;
+        let length = -1;
+        persons[i].clip = new THREE.AnimationClip('rotate', length, persons[i].tracks);
+        persons[i].mixer = new THREE.AnimationMixer(persons[i].rightArm);
+        persons[i].action = persons[i].mixer.clipAction(persons[i].clip);
 
-			persons[i].mesh.getObjectByName('11685_hammer_v1_L3_2').material = gltf.scene.getObjectByName('11685_hammer_v1_L3_2').material.clone();
-			persons[i].mesh.getObjectByName('11685_hammer_v1_L3_2').material.transparent = true;
-			persons[i].mesh.getObjectByName('11685_hammer_v1_L3_2').material.opacity = 0;
-		}
+        persons[i].action.setLoop(THREE.LoopOnce);
+        persons[i].action.play();
 
-		persons[i].rightArm = persons[i].mesh.getObjectByName('Bone');
-		//persons[i].rightArm.material.transparent = true;
-		persons[i].collider = persons[i].mesh.getObjectByName('Collider');
+        scene.add(persons[i].mesh);
+    }
 
-		persons[i].collider.material.transparent = true;
-
-		let rotationKF = new THREE.QuaternionKeyframeTrack('.quaternion', persons[i].times, persons[i].values);
-
-		persons[i].tracks = [rotationKF];
-
-		let length = -1;
-
-		persons[i].clip = new THREE.AnimationClip('rotate', length, persons[i].tracks);
-
-		persons[i].mixer = new THREE.AnimationMixer(persons[i].rightArm);
-
-		persons[i].action = persons[i].mixer.clipAction(persons[i].clip);
-
-		//persons[i].action.clampWhenFinished = true;
-
-		persons[i].action.setLoop(THREE.LoopOnce);
-
-		persons[i].action.play();
-
-		scene.add(persons[i].mesh);
-	}
-
-	persons[setSize - 1].mixer.addEventListener('finished', populationFinished);
-	population = new Population(persons, lukasCollider, setSize, collisionWeight, sounds)
-
+    persons[setSize - 1].mixer.addEventListener('finished', populationFinished);
+    population = new Population(persons, lukasCollider, setSize, collisionWeight, sounds);
 }
 
-
 function populationFinished() {
-	isFinished = true;
-	selection();
-	let newPersons = crossover();
-	let mutatedNewPersons = mutation(newPersons);
-	updatePopulation(mutatedNewPersons);
-	population.resetFitness();
-	population.persons[population.persons.length - 1].mixer.addEventListener('finished', populationFinished);
-	generationIndex++;
-	document.getElementById("generation").innerHTML = "Generation: " + generationIndex;
-	document.getElementById("hits").innerHTML = "Hits: 0";
-	//console.log('finished');
+    isFinished = true;
+    selection();
+    let newPersons = crossover();
+    let mutatedNewPersons = mutation(newPersons);
+    updatePopulation(mutatedNewPersons);
+    population.resetFitness();
+    population.persons[population.persons.length - 1].mixer.addEventListener('finished', populationFinished);
+    generationIndex++;
+    document.getElementById("generation").innerHTML = "Generation: " + generationIndex;
+    document.getElementById("hits").innerHTML = "Hits: 0";
 }
 
 function selection() {
-	fittest = population.getFittest();
-	secondFittest = population.getSecondFittest();
-
-	//let leastFittest = population.persons[population.getLeastFittestIndex()];
-	//console.log(leastFittest.fitness);
+    fittest = population.getFittest();
+    secondFittest = population.getSecondFittest();
 }
 
 function crossover() {
+    let newPersons = [];
+    newPersons.push(fittest.values);
+    newPersons.push(secondFittest.values);
 
-	let newPersons = [];
+    newPersons.push(fittest.getRandomValues());
+    newPersons.push(fittest.getRandomValues());
 
-	//Carry over champions
-	newPersons.push(fittest.values);
-	newPersons.push(secondFittest.values);
+    for (let i = newPersons.length; i < setSize; i++) {
+        let newPersonValues = [];
 
-	//Generate new random persons
-	newPersons.push(fittest.getRandomValues());
-	newPersons.push(fittest.getRandomValues());
+        for (let j = 0; j < fittest.geneLength; j += 4) {
+            let crossoverPoint = Math.floor(Math.random() * 3);
 
-	//Generate new set of people
-	for (let i = newPersons.length; i < setSize; i++) {
-		let newPersonValues = [];
+            let newPersonQuaternionArray = [fittest.values[j], fittest.values[j + 1], fittest.values[j + 2], fittest.values[j + 3]];
+            let newPersonQuaternion = new THREE.Quaternion();
+            newPersonQuaternion.set(fittest.values[j], fittest.values[j + 1], fittest.values[j + 2], fittest.values[j + 3]);
+            let newPersonEuler = new THREE.Euler();
+            newPersonEuler.setFromQuaternion(newPersonQuaternion);
 
-		//Iterate over each Quaternion in long form
-		for (let j = 0; j < fittest.geneLength; j += 4) {
+            let newPersonEulerArray = [newPersonEuler.x, newPersonEuler.y, newPersonEuler.z];
 
-			// Random crossover Point within an Euler (which has 3 values);
-			let crossoverPoint = Math.floor(Math.random() * 3);
+            secondFittestGenes = [secondFittest.values[j], secondFittest.values[j + 1], secondFittest.values[j + 2], secondFittest.values[j + 3]];
 
+            let secondFittestQuaternion = new THREE.Quaternion();
+            secondFittestQuaternion.set(secondFittest.values[j], secondFittest.values[j + 1], secondFittest.values[j + 2], secondFittest.values[j + 3]);
+            let secondFittestEuler = new THREE.Euler();
+            secondFittestEuler.setFromQuaternion(secondFittestQuaternion);
 
-			let newPersonQuaternionArray = [fittest.values[j], fittest.values[j + 1], fittest.values[j + 2], fittest.values[j + 3]];
-			let newPersonQuaternion = new THREE.Quaternion();
-			newPersonQuaternion.set(fittest.values[j], fittest.values[j + 1], fittest.values[j + 2], fittest.values[j + 3]);
-			let newPersonEuler = new THREE.Euler();
-			newPersonEuler.setFromQuaternion(newPersonQuaternion);
+            let secondFittestEulerArray = [secondFittestEuler.x, secondFittestEuler.y, secondFittestEuler.z];
 
-			let newPersonEulerArray = [newPersonEuler.x, newPersonEuler.y, newPersonEuler.z];
+            for (let k = 0; k <= crossoverPoint; k++) {
+                newPersonEulerArray[k] = secondFittestEulerArray[k];
+            }
 
-			secondFittestGenes = [secondFittest.values[j], secondFittest.values[j + 1], secondFittest.values[j + 2], secondFittest.values[j + 3]];
+            newPersonQuaternion.setFromEuler(new THREE.Euler(newPersonEulerArray[0], newPersonEulerArray[1], newPersonEulerArray[2]));
 
-			let secondFittestQuaternion = new THREE.Quaternion();
-			secondFittestQuaternion.set(secondFittest.values[j], secondFittest.values[j + 1], secondFittest.values[j + 2], secondFittest.values[j + 3]);
-			let secondFittestEuler = new THREE.Euler();
-			secondFittestEuler.setFromQuaternion(secondFittestQuaternion);
+            newPersonValues.push(newPersonQuaternion.x);
+            newPersonValues.push(newPersonQuaternion.y);
+            newPersonValues.push(newPersonQuaternion.z);
+            newPersonValues.push(newPersonQuaternion.w);
+        }
 
-			let secondFittestEulerArray = [secondFittestEuler.x, secondFittestEuler.y, secondFittestEuler.z];
-
-			for (let k = 0; k <= crossoverPoint; k++) {
-				newPersonEulerArray[k] = secondFittestEulerArray[k];
-			}
-
-			newPersonQuaternion.setFromEuler(new THREE.Euler(newPersonEulerArray[0], newPersonEulerArray[1], newPersonEulerArray[2]));
-
-			newPersonValues.push(newPersonQuaternion.x);
-			newPersonValues.push(newPersonQuaternion.y);
-			newPersonValues.push(newPersonQuaternion.z);
-			newPersonValues.push(newPersonQuaternion.w);
-		}
-
-		newPersons.push(newPersonValues);
-		//console.log(i);
-	}
-	return newPersons;
-	isFinished = false;
+        newPersons.push(newPersonValues);
+    }
+    return newPersons;
 }
 
 function mutation(newPersons) {
